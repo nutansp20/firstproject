1a9a7b94e03c33dc6ae45ae50a31dfc3eafb0213
nutan p <nutansp20@gmail.com>
2024-08-02 14:31:36 +0530

    defect fixed firsProg

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+
+public class Calculator {
+
+	public static void main(String[] args) {
+
+		int a =6,b=2;
+		MathOperation.Addition(a,b);
+		MathOperation.Multiplication(a,b);
+		MathOperation.Division(a,b);
+		MathOperation.Substraction(a,b);
+		
+	}
+
+}
